189ee887a9ce75dd64e841d4b566bc9e44ca7431
Caleb Morris <caleb.morris.g@gmail.com>
2015-11-07 19:07:23 -0700

    Added description and bumped version v0.1.1

--- ResxTemplates/Properties/AddinInfo.cs
+++ ResxTemplates/Properties/AddinInfo.cs
@@ -5,13 +5,14 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"ResxTemplates", 
 	Namespace = "ResxTemplates",
-	Version = "0.1.0"
+	Version = "0.1.1"
 )]
 
 [assembly:AddinName ("ResxTemplates")]
 [assembly:AddinCategory ("IDE extensions")]
 [assembly:AddinDescription ("ResxTemplates")]
 [assembly:AddinAuthor ("calebmorris")]
+[assembly:AddinDescription ("Resource file templates with different generators.")]
 
 [assembly:ImportAddinFile ("Templates/EmptyResourceFileNoTool.xft.xml")]
 [assembly:ImportAddinFile ("Templates/EmptyResourceFilePublic.xft.xml")]
