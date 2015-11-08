de333ef24ea09bb95d9cd68523e1b0618c61f93b
Caleb Morris <caleb.morris.g@gmail.com>
2015-11-07 18:59:43 -0700

    Initial Commit - Resource file template (public and no generator)

---
+++ ResxTemplates/Properties/AddinInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"ResxTemplates", 
+	Namespace = "ResxTemplates",
+	Version = "0.1.0"
+)]
+
+[assembly:AddinName ("ResxTemplates")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("ResxTemplates")]
+[assembly:AddinAuthor ("calebmorris")]
+
+[assembly:ImportAddinFile ("Templates/EmptyResourceFileNoTool.xft.xml")]
+[assembly:ImportAddinFile ("Templates/EmptyResourceFilePublic.xft.xml")]
+
